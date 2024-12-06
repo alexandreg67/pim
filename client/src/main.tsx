b8348fb730d8d20e45b79643d1dd0cbfb32f988c
alex <alexandre.ag.67@gmail.com>
2024-12-06 18:32:15 +0100

    add MUI configuration and theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material';
 import { theme } from './theme.ts';
 
 createRoot(document.getElementById('root')!).render(
