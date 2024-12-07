87603cb2a3936790430b8e0d0e819b1a16b55756
alex <alexandre.ag.67@gmail.com>
2024-12-07 23:06:22 +0100

    Configure Apollo Client and data sources integration

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,28 +4,36 @@ import StatCard from '../components/StatCard';
 import ProductsByBrandChart from '../components/ProductsByBrandChart';
 import ProductsByCategoryChart from '../components/ProductsByCategoryChart';
 import RecentHistory from '../components/RecentHistory';
+import { useDashboardStatsQuery } from '../generated/graphql-types';
 
 const drawerWidth = 240;
 
 const Dashboard: React.FC = () => {
-  const data = {
-    totalProducts: 10000,
-    productsByBrand: [
-      { categoryOrBrand: 'Dell', count: 840 },
-      { categoryOrBrand: 'HP', count: 1622 },
-      { categoryOrBrand: 'Apple', count: 2527 },
-      { categoryOrBrand: 'Canon', count: 1645 },
-      { categoryOrBrand: 'Samsung', count: 2533 },
-      { categoryOrBrand: 'Seagate', count: 833 },
-    ],
-    productsByCategory: [{ categoryOrBrand: 'Pas de catégorie', count: 10000 }],
-    recentHistory: [
-      { action: 'Produit ajouté', createdAt: '2023-12-05T14:30:00Z' },
-      { action: 'Produit supprimé', createdAt: '2023-12-05T13:00:00Z' },
-    ],
-    pendingCommunications: 5,
-  };
+  const { data, loading, error } = useDashboardStatsQuery();
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const productsByCategoryData = (
+    data?.dashboardStats.productsByCategory || []
+  ).map((category) => ({
+    categoryOrBrand: category.categoryOrBrand || 'Unknown',
+    count: category.count,
+  }));
+
+  const recentHistoryData = (data?.dashboardStats.recentHistory || []).map(
+    (entry) => ({
+      action: entry.action,
+      createdAt: entry.createdAt.toISOString(),
+    })
+  );
+
+  const productsByBrandData = (data?.dashboardStats.productsByBrand || []).map(
+    (el) => ({
+      categoryOrBrand: el.categoryOrBrand || 'Unknown',
+      count: el.count,
+    })
+  );
   return (
     <Box
       sx={{
@@ -38,16 +46,19 @@ const Dashboard: React.FC = () => {
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={4}>
-          <StatCard title="Total Produits" value={data.totalProducts} />
+          <StatCard
+            title="Total Produits"
+            value={data?.dashboardStats.totalProducts || 0}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <RecentHistory history={recentHistoryData} />
         </Grid>
         <Grid item xs={12} sm={8}>
-          <ProductsByBrandChart data={data.productsByBrand} />
+          <ProductsByCategoryChart data={productsByCategoryData} />
         </Grid>
         <Grid item xs={12}>
-          <ProductsByCategoryChart data={data.productsByCategory} />
-        </Grid>
-        <Grid item xs={12}>
-          <RecentHistory history={data.recentHistory} />
+          <ProductsByBrandChart data={productsByBrandData} />
         </Grid>
       </Grid>
     </Box>
